afe7cab3c7993df6957fca20c60144d02c56910e
Datamaverik <verma.pranshu160705@gmail.com>
2025-12-03 11:25:05 +0530

    feat: solve day 3 puzzle

---
+++ src/Day3/MaxSegmentTree.java
@@ -0,0 +1,35 @@
+package Day3;
+
+public class MaxSegmentTree {
+    public long[] num;
+    private final long[] seg;
+
+    MaxSegmentTree(int size, long[] arr) {
+        num = arr;
+        seg = new long[4 * size];
+    }
+
+    public void build(int ind, int low, int high) {
+        if(low == high) {
+            seg[ind] = num[low];
+            return;
+        }
+
+        int mid = low + (high - low) / 2;
+        build(2 * ind + 1, low, mid);
+        build(2 * ind + 2, mid + 1, high);
+        seg[ind] = Math.max(seg[2 * ind + 1], seg[2 * ind + 2]);
+    }
+
+    public long query(int ind, int low, int high, int l, int r) {
+        if(low >= l && high <= r)
+            return seg[ind];
+        if (high < l || low > r)
+            return Long.MIN_VALUE;
+        
+        int mid =  low + (high - low) / 2;
+        long left = query(2 * ind + 1, low, mid, l, r);
+        long right = query(2 * ind + 2, mid + 1, high, l, r);
+        return Math.max(left, right);
+    }
+}
---
+++ src/Day3/SolveDay3.java
@@ -0,0 +1,73 @@
+package Day3;
+
+import java.util.Scanner;
+
+public class SolveDay3 {
+    private static final Scanner in = new Scanner(System.in);
+    private static String[] banks;
+
+    public static void main(String[] args) {
+        readInput();
+        long ans1 = part1();
+        long ans2 = part2();
+        System.out.println(ans1 +  " " + ans2);
+    }
+
+    private static void readInput() {
+        var inputs = new StringBuilder();
+        String input = in.nextLine();
+        while(!input.equals("end")) {
+            inputs.append(input).append(" ");
+            input = in.nextLine();
+        }
+        banks = inputs.toString().trim().split(" ");
+    }
+
+    private static long part1() {
+        long ans = 0;
+        for(String bank : banks) {
+            int len = bank.length();
+            var suffixMax = new long[len];
+            suffixMax[len - 1] = bank.charAt(len - 1) - '0';
+            for(int i = len - 2; i >= 0; i--)
+                suffixMax[i] = Math.max(suffixMax[i + 1], bank.charAt(i) - '0');
+
+            long max = 0;
+            for(int i = 0; i < len - 1; i++) {
+                long num = ((long)(bank.charAt(i) - '0')) * 10 + suffixMax[i + 1];
+                max =  Math.max(max, num);
+            }
+
+            ans += max;
+        }
+        return ans;
+    }
+
+    private static long part2() {
+        long ans = 0;
+        for(String bank : banks) {
+            int len = bank.length();
+            var arr = new long[len];
+            for(int i = 0; i < len; i++) arr[i] = bank.charAt(i) - '0';
+
+            var seg = new MaxSegmentTree(len, arr);
+            seg.build(0, 0, len - 1);
+
+            long max = 0;
+            int digit = 12, lastDigitAt = -1;
+            while(digit > 0) {
+                long maxDigit = seg.query(0, 0, len - 1, lastDigitAt + 1, len - digit);
+                for(int i = lastDigitAt + 1; i <= len - digit; i++)
+                    if(arr[i] == maxDigit) {
+                        lastDigitAt = i;
+                        break;
+                    }
+                max *= 10;
+                max += maxDigit;
+                digit --;
+            }
+            ans += max;
+        }
+        return ans;
+    }
+}
